9d4d254c9ea7c83889ccd8c3da774c1123c4de21
zkitefly <2573874409@qq.com>
2024-05-04 11:20:08 +0000

    update tool

--- mirror-gitee.py
+++ mirror-gitee.py
@@ -1,43 +1,25 @@
 import os
-import json
 
-def replace_in_json(file_path):
+def replace_in_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
-        data = json.load(file)
+        content = file.read()
     
-    # 替换 JSON 数据
-    def replace_recursively(obj):
-        if isinstance(obj, dict):
-            for key, value in obj.items():
-                if isinstance(value, (dict, list)):
-                    replace_recursively(value)
-                elif isinstance(value, str):
-                    obj[key] = value.replace("https://zkitefly.github.io/unlisted-versions-of-minecraft/", "https://gitee.com/bleaker/unlisted-versions-of-minecraft/raw/gitee/")
-        elif isinstance(obj, list):
-            for i in range(len(obj)):
-                if isinstance(obj[i], (dict, list)):
-                    replace_recursively(obj[i])
-                elif isinstance(obj[i], str):
-                    obj[i] = obj[i].replace("https://zkitefly.github.io/unlisted-versions-of-minecraft/", "https://gitee.com/bleaker/unlisted-versions-of-minecraft/raw/gitee/")
-    
-    replace_recursively(data)
+    # 进行替换
+    content = content.replace('https://zkitefly.github.io/unlisted-versions-of-minecraft/', 'https://gitee.com/bleaker/unlisted-versions-of-minecraft/raw/gitee/')
     
     with open(file_path, 'w', encoding='utf-8') as file:
-        json.dump(data, file, ensure_ascii=False)
+        file.write(content)
 
-def process_directory(directory):
+def recursive_replace(directory):
     for item in os.listdir(directory):
         item_path = os.path.join(directory, item)
-        if os.path.isfile(item_path):
-            if item.endswith('.json'):
-                replace_in_json(item_path)
-        elif os.path.isdir(item_path):
-            process_directory(item_path)
+        
+        if os.path.isdir(item_path):
+            recursive_replace(item_path)
+        elif item.endswith('.json'):
+            replace_in_file(item_path)
 
-# 要处理的文件夹路径
-directory_path = 'files'
-
-# 执行递归处理
-process_directory(directory_path)
-
+# 当前目录
+current_dir = os.getcwd()
+recursive_replace(current_dir)
 print("替换完成！")
