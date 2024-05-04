e125e9dda2119e1ee6dc17cc7d9173f646b6a152
zkitefly <2573874409@qq.com>
2024-05-04 11:24:22 +0000

    update

--- mirror-gitee.py
+++ mirror-gitee.py
@@ -1,25 +1,22 @@
 import os
 
-def replace_in_file(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        content = file.read()
+def replace_in_file(file_path, old_text, new_text):
+    with open(file_path, 'r') as file:
+        file_data = file.read()
     
-    # 进行替换
-    content = content.replace('https://zkitefly.github.io/unlisted-versions-of-minecraft/', 'https://gitee.com/bleaker/unlisted-versions-of-minecraft/raw/gitee/')
+    file_data = file_data.replace(old_text, new_text)
     
-    with open(file_path, 'w', encoding='utf-8') as file:
-        file.write(content)
+    with open(file_path, 'w') as file:
+        file.write(file_data)
 
-def recursive_replace(directory):
+def process_files(directory):
     for item in os.listdir(directory):
         item_path = os.path.join(directory, item)
-        
         if os.path.isdir(item_path):
-            recursive_replace(item_path)
+            process_files(item_path)
         elif item.endswith('.json'):
-            replace_in_file(item_path)
+            replace_in_file(item_path, 'https://zkitefly.github.io/unlisted-versions-of-minecraft/', 'https://gitee.com/bleaker/unlisted-versions-of-minecraft/raw/gitee/')
 
-# 当前目录
-current_dir = os.getcwd()
-recursive_replace(current_dir)
-print("替换完成！")
+if __name__ == '__main__':
+    current_directory = os.getcwd()
+    process_files(current_directory)
