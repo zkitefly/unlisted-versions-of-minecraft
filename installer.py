e62953a1fe6a499235f2500bca5fd38348245be9
Zkitefly <2573874409@qq.com>
2024-10-26 09:15:10 +0800

    update

--- installer.py
+++ installer.py
@@ -47,13 +47,13 @@ def fetch_versions():
         for version in versions:
             id = version["id"]
             type = version["type"]
-            url = version["url"]
+            url = version["url"].replace("https://zkitefly.github.io/unlisted-versions-of-minecraft", "https://zkitefly.github.io/unlisted-versions-of-minecraft")
             release_time = version["releaseTime"]
             versions_tree.insert("", "end", values=(id, type, url, release_time))
 
 # 创建主窗口
 root = tk.Tk()
-root.title("unlisted-versions-of-minecraft 版本下载器 - v1.4")
+root.title("unlisted-versions-of-minecraft 版本下载器 - v1.5")
 
 # 创建下载路径输入框和按钮
 export_frame = ttk.Frame(root)
